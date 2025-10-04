6e7f82969d809c064807c47528cba4ffa4c87d7c
SYH <ahmad.zaakir.ameen@gmail.com>
2025-10-04 23:46:25 +0800

    Create gag_lib.lua

---
+++ gag_lib.lua
@@ -0,0 +1,137 @@
+-- This is WindUI, not a custom gui library by SYH
+
+local Library = {}
+Library.__index = Library
+Library.Async = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local UI = Library.Async --// Shortened
+
+local Window = nil
+
+function Library:Setup()
+	local version = LRM_ScriptVersion and "v" .. table.concat(LRM_ScriptVersion:split(""), ".") or "Premium Version"	
+	Window = UI:CreateWindow({
+		Title = "Symphony Hub",
+		Icon = "rbxassetid://73073933429098",
+		Author = (premium and "Premium" or " Grow a Graden") .. " | " .. version,
+		Folder = "SYH-GAG",
+		Size = UDim2.fromOffset(580, 460),
+		Transparent = true,
+		Theme = "Dark",
+		Resizable = true,
+		SideBarWidth = 200,
+		Background = "",
+		BackgroundImageTransparency = 0.42,
+		HideSearchBar = true,
+		ScrollBarEnabled = false,
+		User = {
+			Enabled = true,
+			Anonymous = false,
+			Callback = function()
+				print("clicked")
+			end,
+		},
+	})
+
+	Window:EditOpenButton({
+    Title = "Symphony Hub - Open",
+    Icon = "monitor",
+    CornerRadius = UDim.new(0, 6),
+    StrokeThickness = 2,
+    Color = ColorSequence.new(Color3.fromRGB(30, 30, 30), Color3.fromRGB(255, 255, 255)),
+    Draggable = true,
+})
+
+	return Window
+end
+
+function Library:CreateTab(Name, Icon)
+	local Window = Window or self:Setup()
+
+	if not Window then
+		error("[Library]: Failed to find Window")
+		return
+	end
+
+	local Tab = Window:Tab({
+		Title = Name,
+		Icon = Icon,
+		Locked = false,
+	})
+
+	return Tab
+end
+
+function Library:CreateSection(Tab, Title, Size)
+	local Section = Tab:Section({
+		Title = Title,
+		TextXAlignment = "Left",
+		TextSize = Size or 17,
+	})
+
+	return Section
+end
+
+function Library:CreateToggle(Tab, Table)
+	local Toggle = Tab:Toggle(Table)
+
+	return Toggle
+end
+
+function Library:CreateButton(Tab, Table)
+	local Button = Tab:Button(Table)
+
+	return Button
+end
+
+function Library:CreateSlider(Tab, Table)
+	local Slider = Tab:Slider(Table)
+
+	return Slider
+end
+
+function Library:CreateDropdown(Tab, Table)
+	local Dropdown = Tab:Dropdown(Table)
+
+	return Dropdown
+end
+
+function Library:CreateInput(Tab, Table)
+	local Input = Tab:Input(Table)
+
+	return Input
+end
+
+--// Special Setups
+function Library:SetupAboutUs(AboutUs)
+	local Window = Window or self:Setup()
+
+	if not Window then
+		error("[Library]: Failed to find Window")
+		return
+	end
+
+	-- AboutUs:Paragraph({
+	-- 	Title = "Who Are We?",
+	-- 	Icon = "user-circle",
+	-- 	Desc = "Symphony Hub is built by two expert Roblox developers, we deliver smooth Lua scripts, fast execution, and modern UI—supporting games like 99 Nights in a Forest, and more!.
+",
+	-- })
+
+	AboutUs:Paragraph({
+		Title = "Discord Invites",
+		Icon = "discord",
+		Desc = "Join our discord for updates and support!",
+	})
+
+	AboutUs:Button({
+		Title = "Discord Link (Click to Copy)",
+		Icon = "link",
+		Callback = function()
+			setclipboard("https://discord.gg/U7WcA3SVV9")
+			Library:Notify({ Title = "Copied!", Content = "Discord link copied!", Duration = 3 })
+		end,
+	})
+end
+
+return Library
